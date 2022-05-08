0f4590282f3703aa8e8e0b219e6582018aa32eef
grishasss <grisha.voronin2018@yandex.ru>
2022-05-08 14:25:02 +0300

    1

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
