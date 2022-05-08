cc3c84c88880d44069625fc3a15d3a3688bfd1e6
grishasss <grisha.voronin2018@yandex.ru>
2022-05-08 18:50:39 +0300

    start

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,33 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
+#include <FS.h>
+#include <WiFiUdp.h>
+
+
+
+
+#define R1 1000
+#define R2 1000
+
+
+Adafruit_MPU6050 mpu;
+
+float get_voltage(){
+    return (analogRead(A0)) / 1024 * (R1 + R2) / R2;    
+}
+
+
+
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+   Serial.begin(115200);
+   bool mpu_begin = mpu.begin();
+   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
