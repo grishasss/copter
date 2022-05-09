11d9f576ee42e29601217b8319dc0be06a15d7d6
grishasss <grisha.voronin2018@yandex.ru>
2022-05-09 17:48:38 +0300

    add motor control

--- src/motor.cpp
+++ src/motor.cpp
@@ -1,1 +1,16 @@
 #include "motor.h"
+// #include "ESP8266_PWM.h"
+
+MOTORS::MOTORS(){
+
+}
+void MOTORS::stop(int MOTOR_ID){
+    power[MOTOR_ID] = 0;
+}
+
+void MOTORS::stop_all(){
+
+}
+void MOTORS::set_power_to(int MOTOR_ID , int Power){
+    power[MOTOR_ID] = Power;
+}
--- src/motor.h
+++ src/motor.h
@@ -1,1 +1,20 @@
 #include <Arduino.h>
+#include "ESP8266_PWM.hpp"
+
+#define MOTOR_1_PIN 1
+#define MOTOR_2_PIN 2
+#define MOTOR_3_PIN 3
+#define MOTOR_4_PIN 4
+
+class MOTORS{
+    public:
+    ESP8266_PWM ISR_PWM;
+    MOTORS();
+    int power[5];
+    
+    
+    void stop_all();
+    void stop(int MOTOR_ID);
+    void set_power_to(int MOTOR_ID , int power);
+
+};
