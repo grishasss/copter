c65002b3c3a710d0086ebf62e4cf8e2e5f84f7a8
grishasss <grisha.voronin2018@yandex.ru>
2022-05-09 09:19:08 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 
 
 
@@ -16,7 +17,7 @@
 
 
 ESP8266WebServer server(80);
-
+WebSocketsServer webSocket(81);
 
 Adafruit_MPU6050 mpu;
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
@@ -29,6 +30,46 @@ float get_voltage(){
     return (analogRead(A0)) / 1024 * (R1 + R2) / R2;    
 }
 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+    return;
+}
+
+
+
+void start_WebSocket() {
+  webSocket.begin();                         
+  webSocket.onEvent(webSocketEvent);        
+  Serial.println("WebSocket server started.");
+}
+
+bool start_mpu(){
+    if(mpu.begin()){
+        Serial.println("MPU is OK");
+        return true;
+    }
+    Serial.println("MPU is FAILD");
+    return false;
+}
+
+bool start_mag(){
+    if(mag.begin()){
+        Serial.println("MAG is OK");
+    }
+    Serial.println("MAG is FAILD");
+    return false;
+}
+
+bool start_lox(){
+    if(mpu.begin()){
+        Serial.println("LOX is OK");
+        return true;
+    }
+    Serial.println("LOX is FAILD");
+    return false;
+}
+
+
+
 
 
 
@@ -38,18 +79,18 @@ float get_voltage(){
 void setup() {
     Serial.begin(115200);
     WiFi.softAP("efs", "12345678");
-    bool is_mpu_begin = mpu.begin();
-    bool is_mag_begin = mag.begin();
-    bool is_lox_begin = lox.begin();
-    Serial.print("mpu :");
-    Serial.println(is_mpu_begin);
-    Serial.print("mag :");
-    Serial.println(is_mag_begin);
-    Serial.print("lox :");
-    Serial.println(is_lox_begin);
     
+    bool is_mpu_begin = start_mpu();
+    bool is_mag_begin = start_mag();
+    bool is_lox_begin = start_lox();
+    
+    start_WebSocket();
+    
+
 }
 
 void loop() {
   
 }
+
+
