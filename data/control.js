81572af820b052b725987aa83e207a3fdc3c02ef
grishasss <grisha.voronin2018@yandex.ru>
2022-06-09 16:04:45 +0300

    web fix

---
+++ data/control.js
@@ -0,0 +1,43 @@
+// document.documentElement.requestFullScreen();
+var joy = new JoyStick('joyDiv',{
+    // The ID of canvas element
+    title: 'joystick',
+    // width/height
+    width: undefined,
+    height: undefined,
+    // Internal color of Stick
+    internalFillColor: '#00AA00',
+    // Border width of Stick
+    internalLineWidth: 2,
+    // Border color of Stick
+    internalStrokeColor: '#003300',
+    // External reference circonference width
+    externalLineWidth: 2,
+    //External reference circonference color
+    externalStrokeColor: '#008000',
+    // Sets the behavior of the stick
+    autoReturnToCenter: true
+    
+});
+
+
+var joy1 = new JoyStick('joy1Div',{
+    // The ID of canvas element
+    title: 'joystick',
+    // width/height
+    width: undefined,
+    height: undefined,
+    // Internal color of Stick
+    internalFillColor: '#00AA00',
+    // Border width of Stick
+    internalLineWidth: 2,
+    // Border color of Stick
+    internalStrokeColor: '#003300',
+    // External reference circonference width
+    externalLineWidth: 2,
+    //External reference circonference color
+    externalStrokeColor: '#008000',
+    // Sets the behavior of the stick
+    autoReturnToCenter: true
+    
+});
