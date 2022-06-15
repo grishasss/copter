c20ffd370a5bdbf443d2a67b56edc8aa0fbf8bc8
grishasss <grisha.voronin2018@yandex.ru>
2022-06-15 14:14:38 +0300

    math fix

--- src/math_copter.cpp
+++ src/math_copter.cpp
@@ -7,3 +7,27 @@ MATH::MATH(){
 float MATH::get_acceleration(float new_angle , float tec_angle){
     return new_angle - tec_angle;
 }
+
+
+void MATH::calc_acro(){
+
+}
+
+void MATH::calc_auto(){
+    
+}
+
+
+void MATH::loop(){
+    real_altitude = Sensors->altitude * cos(Sensors->kren) * cos(Sensors->tangage);
+    
+    switch (mode){
+    case 0:
+        calc_acro();
+        break;
+    case 1:
+        calc_auto();
+    default:
+        break;
+    }
+}
--- src/math_copter.h
+++ src/math_copter.h
@@ -10,8 +10,13 @@ public:
 
 
     MATH();
-    
+
+    uint8_t mode = 0;
+    float real_altitude = 0;
     float get_acceleration(float new_angle , float tec_angle);
+    void calc_acro();
+    void calc_auto();
+    void loop();
 };
 
 
